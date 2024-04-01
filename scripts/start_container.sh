afb0de604f0b0d492c09554f8798e5ea9b09d10b
Salmankhan7798 <109876884+Salmankhan7798@users.noreply.github.com>
2024-04-01 08:45:54 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull salmankhan7798/simple-python-flask-app
+docker pull salmankhan7798/my-html-project:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 salmankhan7798/simple-python-flask-app
+docker run -d -p 8080:80 salmankhan7798/my-html-project:latest
